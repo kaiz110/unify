c8824c48fc55d76ab718ba9bc88013f81746ce68
DESKTOP-G28C0PF\hk <dinhkhiem110@gmail.com>
2021-01-03 20:58:10 +0700

    bottom tab

--- App.js
+++ App.js
@@ -1,31 +1,71 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-//
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {MaterialCommunityIcons,AntDesign} from '@expo/vector-icons';
+//screens
+import ApiScreen from "./src/screens/ApiScreen";
+import ComponentScreen from "./src/screens/ComponentScreen";
 
-const BottomTab = createMaterialBottomTabNavigator()
+const BottomTab = createBottomTabNavigator()
 const Stack = createStackNavigator()
 
 //nested screens
 const ApiS = ()=>(
-    <Stack>
-        <Stack.Screen name="" component={} />
-    </Stack>
+    <Stack.Navigator>
+        <Stack.Screen
+            name="HomeApi"
+            component={ApiScreen}
+            options={{
+                title: "API"
+            }}
+        />
+    </Stack.Navigator>
 )
 
 const ComponentS = ()=>(
-    <Stack>
-        <Stack.Screen name="" component={} />
-    </Stack>
+    <Stack.Navigator>
+        <Stack.Screen
+            name="HomeComponent"
+            component={ComponentScreen}
+            options={{
+                title: "Component"
+            }}
+        />
+    </Stack.Navigator>
 )
 
-const App = ()=> (
-    <BottomTab>
-        <BottomTab.Screen name="Api" component={ApiS}/>
-        <BottomTab.Screen name="Component" component={ComponentS}/>
-    </BottomTab>
+const App = ()=>(
+    <NavigationContainer>
+        <BottomTab.Navigator
+            tabBarOptions={{
+                activeTintColor: '#130f40',
+                inactiveTintColor: '#95afc0',
+                showLabel: false
+            }}
+        >
+            <BottomTab.Screen
+                name="Api"
+                component={ApiS}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <AntDesign name="API" color={color} size={size}/>
+                    )
+                }}
+            />
+            <BottomTab.Screen
+                name="Component"
+                component={ComponentS}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons name="react" color={color} size={size}/>
+                    )
+                }}
+            />
+        </BottomTab.Navigator>
+    </NavigationContainer>
 )
 
 export default App

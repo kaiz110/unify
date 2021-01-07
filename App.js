bda2a72e6da7ab3306140020e623ea51f54b8e7f
DESKTOP-G28C0PF\hk <dinhkhiem110@gmail.com>
2021-01-07 17:10:02 +0700

    change bottom tab to material bottom tab

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {UIManager} from 'react-native';
+import {Provider as PaperProvider,BottomNavigation} from 'react-native-paper';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+
 import {MaterialCommunityIcons,AntDesign} from '@expo/vector-icons';
 //screens
 import ApiScreen from "./src/screens/ApiScreen";
@@ -16,7 +18,7 @@ import SlideScreen from "./src/screens/componentScreens/SlideScreen";
 import TimerScreen from "./src/screens/componentScreens/TimerScreen";
 import LoveCalcScreen from "./src/screens/apiScreens/LoveCalcScreen";
 
-const BottomTab = createBottomTabNavigator()
+const BottomTab = createMaterialBottomTabNavigator()
 const Stack = createStackNavigator()
 
 // ui for layout animation on android
@@ -60,32 +62,38 @@ const ComponentS = ()=>(
 const App = ()=>(
     <NavigationContainer>
         <BottomTab.Navigator
-            tabBarOptions={{
-                activeTintColor: '#130f40',
-                inactiveTintColor: '#95afc0',
-                showLabel: false
-            }}
+            initialRouteName="Api"
+            activeColor="#f0edf6"
+            inactiveColor="#3e2465"
+            shifting
+            barStyle={{ backgroundColor: '#694fad' }}
         >
             <BottomTab.Screen
                 name="Api"
                 component={ApiS}
                 options={{
-                    tabBarIcon: ({color, size}) => (
-                        <AntDesign name="API" color={color} size={size}/>
-                    )
+                    tabBarIcon: ({color}) => (
+                        <AntDesign name="API" color={color} size={24}/>
+                    ),
+                    tabBarColor: '#00b894'
                 }}
             />
             <BottomTab.Screen
                 name="Component"
                 component={ComponentS}
                 options={{
-                    tabBarIcon: ({color, size}) => (
-                        <MaterialCommunityIcons name="react" color={color} size={size}/>
-                    )
+                    tabBarIcon: ({color}) => (
+                        <MaterialCommunityIcons name="react" color={color} size={24}/>
+                    ),
+                    tabBarColor: '#6c5ce7'
                 }}
             />
         </BottomTab.Navigator>
     </NavigationContainer>
 )
 
-export default App
+export default ()=>{
+    return <PaperProvider>
+        <App/>
+    </PaperProvider>
+}
